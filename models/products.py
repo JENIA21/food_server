34ae2b805bccf9553b08f274c7913e15dc9e7ae3
Жавохирбек <72182135+JENIA21@users.noreply.github.com>
2023-02-19 19:48:40 +0500

    fixed bugs in models

--- models/products.py
+++ models/products.py
@@ -1,16 +1,17 @@
-from sqlalchemy import Integer, String, Column, ForeignKey
 from .recipes import recipes
 import sqlalchemy
 
 metadata = sqlalchemy.MetaData()
 
 recipes_and_product = sqlalchemy.Table('recipes_and_product', metadata,
-                                       Column('recipes_id', Integer(), ForeignKey(recipes.c.id)),
-                                       Column('products_id', Integer(), ForeignKey('product.id'))
+                                       sqlalchemy.Column('recipes_id', sqlalchemy.Integer(),
+                                                         sqlalchemy.ForeignKey(recipes.c.id)),
+                                       sqlalchemy.Column('products_id', sqlalchemy.Integer(),
+                                                         sqlalchemy.ForeignKey('product.id'))
                                        )
 
 product = sqlalchemy.Table('product', metadata,
-                           Column('id', Integer(), primary_key=True),
-                           Column('name', String(100), ForeignKey('product.id')),
-                           Column('recipes_id', Integer(), ForeignKey(recipes.c.id)),
+                           sqlalchemy.Column('id', sqlalchemy.Integer(), primary_key=True),
+                           sqlalchemy.Column('name', sqlalchemy.String(100)),
+                           sqlalchemy.Column('recipes_id', sqlalchemy.Integer(), sqlalchemy.ForeignKey(recipes.c.id)),
                            )
